4d537daf6f6f3a1959f07f5ebcb2435ea243c0e7
Jake Pople <jakepople@hotmail.co.uk>
2022-08-22 11:55:00 +0100

    using @answer instead of params[:answer]

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,12 +2,12 @@ class QuestionsController < ApplicationController
   def ask; end
 
   def answer
-    params[:answer] = if params[:question] == 'I am going to work'
-                        'Great!'
-                      elsif params[:question].end_with?('?')
-                        'Silly question, get dressed and go to work!'
-                      else
-                        'I don\'t care, get dressed and go to work!'
-                      end
+    @answer = if params[:question] == 'I am going to work'
+                'Great!'
+              elsif params[:question].end_with?('?')
+                'Silly question, get dressed and go to work!'
+              else
+                'I don\'t care, get dressed and go to work!'
+              end
   end
 end
